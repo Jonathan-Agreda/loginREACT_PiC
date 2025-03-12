0f90933ce0974e24c57a846d2c63d2a0b9578a02
Jonathan-Agreda <flavioagreda1990@gmail.com>
2025-03-11 23:19:46 -0500

    css modules

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,13 @@
-import './Header.css'
+import styles from './Header.module.css'
 
 import wonkaLogo from '../../assets/WonkaLogo.png'
 
 export const Header = () => {
     return (
-        <header className='header'>
-            <img src={wonkaLogo} alt="WonkaLogo" />
-            <div className='register'>
-                <button className='register-button'>Register</button>
+        <header className={styles.header}>
+            <img className={styles.headerImg} src={wonkaLogo} alt="WonkaLogo" />
+            <div className={styles.register}>
+                <button className={styles.registerButton}>Register</button>
             </div>
         </header>
     )
