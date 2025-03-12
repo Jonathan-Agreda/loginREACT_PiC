9d65834fc41123f5ddb14c3f41ceddf415b22b9c
Jonathan-Agreda <flavioagreda1990@gmail.com>
2025-03-12 00:44:24 -0500

    css con stayled components

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,31 @@
 import './Header.css'
 
+import stayled from 'styled-components'
+
 import wonkaLogo from '../../assets/WonkaLogo.png'
+
+const StyledHeader = stayled.header`
+    width: 100%;
+    padding: 20px;
+    background-color: #684fa3;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+`
+const StyledImg = stayled.img`
+    width: 140px;
+    margin-left: 40px;
+`
 
 export const Header = () => {
     return (
-        <header className='header'>
-            <img src={wonkaLogo} alt="WonkaLogo" />
+        <StyledHeader>
+            <StyledImg src={wonkaLogo} alt="WonkaLogo" />
             <div className='register'>
                 <button className='register-button'>Register</button>
             </div>
-        </header>
+        </StyledHeader>
     )
 }
