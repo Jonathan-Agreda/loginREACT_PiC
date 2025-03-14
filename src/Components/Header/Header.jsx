930c3a5536925b3f52e3d33ebb28d7dce644bdb1
Jonathan-Agreda <flavioagreda1990@gmail.com>
2025-03-14 00:13:02 -0500

    instalacion de tailwindcss y extension

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,8 +4,8 @@ import wonkaLogo from '../../assets/WonkaLogo.png'
 
 export const Header = () => {
     return (
-        <header className='header'>
-            <img src={wonkaLogo} alt="WonkaLogo" />
+        <header className='w-full p-5 bg-purple-700 flex flex-row justify-between items-center shadow-md'>
+            <img class="w-36 ml-10" src={wonkaLogo} alt="WonkaLogo" />
             <div className='register'>
                 <button className='register-button'>Register</button>
             </div>
